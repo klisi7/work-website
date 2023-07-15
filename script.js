4c012f51b473d79c849e00de25626a1fe2791996
klisi7 <117843390+klisi7@users.noreply.github.com>
2023-07-15 20:34:32 +0200

    Update script.js

--- script.js
+++ script.js
@@ -27,3 +27,42 @@ let hiddenElements = document.querySelectorAll(".hidden");
 hiddenElements.forEach(el => observer.observe(el));
 
 
+let carousel = document.querySelector(".carousel");
+let arrowIcons = document.querySelectorAll("#galery_wrapper i");
+let firstImg = carousel.querySelectorAll("img")[0];
+
+let isDragStart = false, prevPageX, prevScrollLeft;
+let firstImgWidth = firstImg.clientWidth + 14;
+
+
+arrowIcons.forEach(target =>{
+    target.onclick = () =>{
+        carousel.scrollLeft += target.id == "left" ? -firstImgWidth : firstImgWidth;
+    }
+})
+
+let dragStart = (e) =>{
+    isDragStart = true;
+    prevPageX = e.pageX;
+    prevScrollLeft = carousel.scrollLeft;
+}
+
+let dragging = (e) =>{
+    if(!isDragStart) return;
+    e.preventDefault();
+    carousel.scrollLeft = e.pageX;
+
+    let positionDiff = e.pageX -prevPageX;
+    carousel.scrollLeft = prevScrollLeft - positionDiff;
+
+    carousel.classList.add("dragging");
+}
+
+let dragStop = () =>{
+    isDragStart = false;
+    carousel.classList.remove("dragging");
+}
+
+carousel.addEventListener("mousedown", dragStart)
+carousel.addEventListener("mousemove", dragging)
+carousel.addEventListener("mouseup", dragStop)
